543c494db13e6e6754460d9a8f5332d340d0e6ca
김동오 <dongo1450@gmail.com>
2023-01-29 02:02:01 +0900

    Practicing2

--- 백준/연습.py
+++ 백준/연습.py
@@ -6,3 +6,14 @@
 # for i in range(A):
 #     a, b = map(int, sys.stdin.readline().split())
 #     print(a+b)
+
+
+
+
+# 예쁘게 A + B 출력
+
+# T = int(input())
+
+# for i in range(1, T+1):
+#     A, B = map(int, input().split())
+#     print(f'Case #{i}: {A+B}')
