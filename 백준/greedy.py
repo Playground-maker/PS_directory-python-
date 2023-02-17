001625ae7d643a6dcbb832236446c60b2c754eb4
김동오 <dongo1450@gmail.com>
2023-02-18 04:19:21 +0900

    practicing...

--- 백준/greedy.py
+++ 백준/greedy.py
@@ -28,4 +28,26 @@
     
 # print(result) 
 
- 
+
+
+# 1이 될 때 까지 case 1
+
+n, k = map(int, input().split())
+result = 0
+
+while True:
+  target = (n//k) * k
+  result += (n - target)
+  n = target
+  
+  if n < k:
+    break
+  
+  result += 1
+  n = n // k
+  
+result = result + (n - 1)
+print(result)  
+
+
+
