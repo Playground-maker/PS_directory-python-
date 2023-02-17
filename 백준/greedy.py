7d3362204be7d9ce2493fe6e86146c7decbd323d
김동오 <dongo1450@gmail.com>
2023-02-18 04:27:35 +0900

    hi2

--- 백준/greedy.py
+++ 백준/greedy.py
@@ -32,22 +32,46 @@
 
 # 1이 될 때 까지 case 1
 
-n, k = map(int, input().split())
-result = 0
+# n, k = map(int, input().split())
+# result = 0
 
-while True:
-  target = (n//k) * k
-  result += (n - target)
-  n = target
+# while True:
+#   target = (n//k) * k
+#   result += (n - target)
+#   n = target
   
-  if n < k:
-    break
+#   if n < k:
+#     break
   
-  result += 1
-  n = n // k
+#   result += 1
+#   n = n // k
   
-result = result + (n - 1)
-print(result)  
+# result = result + (n - 1)
+# print(result)  
+
+
+# case 2
+
+# n, k = map(int, input().split())
+# result = 0
+
+# while n >= k:
+  
+#   while n % k != 0:
+#     n = n - 1
+#     result += 1
+  
+#   n = n // k
+#   result += 1
+  
+# while n > 1:
+#   n = n - 1
+#   result += 1
+  
+# print(result)   
+
+
+ 
 
 
 
