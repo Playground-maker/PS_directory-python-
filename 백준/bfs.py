fb850eff207924790eb75b0162a46046ca6d7556
김동오 <dongo1450@gmail.com>
2023-02-20 07:43:40 +0900

    hi3

--- 백준/bfs.py
+++ 백준/bfs.py
@@ -1,18 +1,50 @@
 # BFS 
 
-from collections import deque
 
-queue = deque()
 
-queue.append(5)
-queue.append(2)
-queue.append(3)
-queue.append(7)
-queue.popleft()
-queue.append(1)
-queue.append(4)
-queue.popleft()
 
-print(queue)
-queue.reverse()
-print(queue)
+
+
+
+# from collections import deque
+
+# queue = deque()
+
+# queue.append(5)
+# queue.append(2)
+# queue.append(3)
+# queue.append(7)
+# queue.popleft()
+# queue.append(1)
+# queue.append(4)
+# queue.popleft()
+
+# print(queue)
+# queue.reverse()
+# print(queue)
+
+
+# 재귀함수
+
+# def recursive_function(i):
+#   if i == 10:
+#     return
+#   print(i, '번째 재귀 함수에서', i+1, '번째 재귀 함수를 호출합니다.')
+#   recursive_function(i+1)
+#   print(i, '번째 재귀 함수를 종료합니다')
+  
+# recursive_function(1)  
+
+
+# def factorial_iterative(n):
+#   result = 1
+#   for i in range(1, n+1):
+#     result *= i
+#   return result 
+
+# def factorial_recursive(n):
+#   if n <= 1:
+#     return 1
+#   return n * factorial_recursive(n-1)
+
+ 
--- 백준/dfs.py
+++ 백준/dfs.py
@@ -1,16 +1,49 @@
 # dfs 풀이(코테)
 
+
+# Adjacency matrix - graph
+
+INF = 99999999
+
+graph = [
+  [0, 7, 5], 
+  [7, 0, INF],
+  [5, INF, 0]
+]
+
+print(graph)
+
+
+# Adjacency list - graph
+
+# 행이 3개인 2차원 리스트로 인접 리스트 표현
+graph = [[] for _ in range(3)]
+
+# 노드 0에 연결된 노드 정보 저장(노드, 거리)
+graph[0].append((1, 7))
+graph[0].append((2, 5))
+
+# 노드 1에 연결된 노드 정보 저장
+graph[1].append((0, 7))
+
+# 노드 2에 연결된 노드 정보 저장
+graph
+
+
+
+
+
 # <스택 자료구조>
 
-stack = []
-stack.append(5)
-stack.append(2)
-stack.append(3)
-stack.append(7)
-stack.pop()
-stack.append(1)
-stack.append(4)
-stack.pop()
+# stack = []
+# stack.append(5)
+# stack.append(2)
+# stack.append(3)
+# stack.append(7)
+# stack.pop()
+# stack.append(1)
+# stack.append(4)
+# stack.pop()
 
-print(stack)
-print(stack[::-1])
+# print(stack)
+# print(stack[::-1])
